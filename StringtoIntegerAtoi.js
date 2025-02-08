b01c70b32f76939a0197821002fe991450bae28e
Anjalit9714 <anjalit9714@gmail.com>
2025-02-08 09:37:07 +0530

    Update

---
+++ StringtoIntegerAtoi.js
@@ -0,0 +1,17 @@
+let myAtoi = function (s) {
+    const ans = Number.parseInt(s);
+    if (ans) {
+        if (ans <= -2147483648) {
+            return -2147483648
+        }
+        else if (ans >= 2147483647) {
+            return 2147483647
+        }
+        else {
+            return ans
+        }
+    }
+    return 0;
+}
+
+console.log(myAtoi(" -042"))
