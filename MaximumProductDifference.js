18e67679b701e8fdec6dfc2e9a9ce3adeee608b5
Anjalit9714 <anjalit9714@gmail.com>
2025-02-11 11:26:58 +0530

    new

---
+++ MaximumProductDifference.js
@@ -0,0 +1,8 @@
+let maxProductDifference = function(nums) {
+
+    nums.sort((a,b)=>a-b)
+   
+    return (nums[nums.length-1]*nums[nums.length-2])-(nums[0]*nums[1])
+}
+
+console.log(maxProductDifference([5,6,2,7,4]));
